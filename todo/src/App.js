8db72f7770f6a2360696cbf689392612789955c0
Ben <thisbenrogers@gmail.com>
2019-08-13 15:23:17 -0500

    :art: Adds Components

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
+import TodoList from './components/TodoList';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Todo App</h1>
+      <TodoList />
     </div>
   );
 }
---
+++ todo/src/components/TodoList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Todo from './Todo';
+
+const TodoList = () => {
+    return (
+        <>
+            <p>todo item</p>
+            <Todo />
+        </>
+    )
+}
+
+export default TodoList;
